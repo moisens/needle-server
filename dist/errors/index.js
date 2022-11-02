3b39788486547aa9ab4d9632eb3595418941eb6c
moise <mose.ns@hotmail.com>
2022-11-02 23:08:12 +0100

    errors functionality

---
+++ dist/errors/index.js
@@ -0,0 +1,4 @@
+import CustomAPIError from "./custom-api.js";
+import BadRequestError from "./bad-request.js";
+import NotFoundError from "./not-found.js";
+export { CustomAPIError, BadRequestError, NotFoundError };
