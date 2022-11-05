c2e5b7d71a7b9d0b3f6e1947fde8e3b96544c724
moise <mose.ns@hotmail.com>
2022-11-06 00:27:43 +0100

    added serialized func

--- dist/errors/not-found.js
+++ dist/errors/not-found.js
@@ -1,10 +1,13 @@
 import { StatusCodes } from "http-status-codes";
 import CustomAPIError from "./custom-api";
 class NotFoundError extends CustomAPIError {
-    statusCode;
+    statusCode = StatusCodes.NOT_FOUND;
     constructor(message) {
         super(message);
-        this.statusCode = StatusCodes.NOT_FOUND;
+        //this.statusCode = StatusCodes.NOT_FOUND;
+    }
+    serializeErrors() {
+        return [{ message: this.message }];
     }
 }
 export default NotFoundError;
--- errors/custom-api.ts
+++ errors/custom-api.ts
@@ -1,9 +1,12 @@
 abstract class CustomAPIError extends Error {
+  abstract statusCode: number;
   constructor(message: string) {
     super(message);
     Object.setPrototypeOf(this, CustomAPIError.prototype)
   }
-  //abstract serializeErrors(): { message: string, field?: string }[]
+  abstract serializeErrors(): { 
+    message: string, field?: string 
+  }[]
 }
 
 export default CustomAPIError;
