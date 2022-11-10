de9d723a048655ad969e555cbc503579caf8ddd9
moise <mose.ns@hotmail.com>
2022-11-10 22:27:32 +0100

    create token types

--- dist/utils/createTokenUser.js
+++ dist/utils/createTokenUser.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 const createTokenuser = (user) => {
-    return { name: user.name, userId: new mongoose.Types.ObjectId(), role: user.role };
+    return {
+        name: user.name,
+        userId: new mongoose.Types.ObjectId(),
+        role: user.role,
+    };
 };
 export default createTokenuser;
--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,20 +1,33 @@
 import { Response } from "express";
 import jsonwebtoken from "jsonwebtoken";
 import { UserDocument } from "../models/Users";
+import mongoose from "mongoose";
 
+export interface TokenUser {
+  name: string;
+  userId: mongoose.Types.ObjectId;
+  role: string;
+}
 
 const jwt = jsonwebtoken;
 
-const createJWT = ({ payload }: { payload: UserDocument }) => {
+const createJWT = ({ payload }: { payload: TokenUser }) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET!, {
-    expiresIn: process.env.JWT_LIFETIME!
-  })
+    expiresIn: process.env.JWT_LIFETIME!,
+  });
   return token;
-}
+};
 
-const isTokenValid = ({ token }: { token: string }) => jwt.verify(token, process.env.JWT_SECRET!);
+const isTokenValid = ({ token }: { token: string }) =>
+  jwt.verify(token, process.env.JWT_SECRET!);
 
-const attachCookiesToResponse = ({ res, user }: { res: Response, user: UserDocument }) => {
+const attachCookiesToResponse = ({
+  res,
+  user,
+}: {
+  res: Response;
+  user: TokenUser;
+}) => {
   const token = createJWT({ payload: user });
   const oneDay = 1000 * 60 * 60 * 24;
   res.cookie("token", token, {
@@ -23,11 +36,6 @@ const attachCookiesToResponse = ({ res, user }: { res: Response, user: UserDocum
     secure: process.env.NODE_ENV === "production",
     signed: true,
   });
-}
+};
 
-export {
-  createJWT,
-  isTokenValid,
-  attachCookiesToResponse
-}
-
+export { createJWT, isTokenValid, attachCookiesToResponse };
