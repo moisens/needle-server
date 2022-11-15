8d42c65248c51281e9d0a5179dd1fe16df6a7710
moise <mose.ns@hotmail.com>
2022-11-15 23:47:05 +0100

    update user functionality part 1

--- dist/controllers/userController.js
+++ dist/controllers/userController.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, NotFoundError } from "../errors/index.js";
 import User from "../models/Users.js";
+import createTokenuser from "../utils/createTokenUser.js";
+import { attachCookiesToResponse } from "../utils/jwt.js";
 const getAllUsers = async (req, res) => {
     const users = await User.find({ role: "user" }).select("-password");
     res.status(StatusCodes.OK).json({ users });
@@ -15,15 +17,12 @@ const getSingleUser = async (req, res) => {
 };
 const updateUser = async (req, res) => {
     const { name, lastname, email } = req.body;
+    const user = await User.findOneAndUpdate({ _id: req.user.userId });
     if (!name || !lastname || !email)
-        throw new BadRequestError("Invalid credentials!");
-    const user = await User.findOneAndUpdate({ _id: req.params.userId }, { name, lastname, email }, {
-        new: true,
-        runValidators: true,
-    });
-    //const tokenUser = createTokenuser(user);
-    //attachCookiesToResponse({ res, user: tokenUser })
-    res.status(StatusCodes.OK).json({ user });
+        throw new BadRequestError("Please provide all values!");
+    const tokenUser = createTokenuser(user);
+    attachCookiesToResponse({ res, user: tokenUser });
+    res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const updateUserPassword = async (req, res) => {
     console.log("updating user password!");
